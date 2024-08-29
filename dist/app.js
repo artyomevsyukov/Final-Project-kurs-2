14fb01d5bcbcd66aee78f2eb8f24dab1f0e68698
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-08-29 15:29:07 +0300

    view and route

---
+++ dist/app.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    class AbstractView {
+        constructor() {
+            this.app = document.getElementById("root");
+        }
+
+        setTitle(title) {
+            document.title = title;
+        }
+        render() {
+            return;
+        }
+        destroy() {
+            return;
+        }
+    }
+
+    class MainView extends AbstractView {
+        constructor() {
+            super();
+            this.setTitle = "Поиск книг";
+        }
+
+        render() {
+            const main = document.createElement("div");
+            main.innerHTML = "Tecт";
+            this.app.innerHTML = "";
+            this.app.append(main);
+        }
+    }
+
+    class App {
+        routes = [{ path: "", view: MainView }];
+        constructor() {
+            window.addEventListener("hashchange", this.route.bind(this));
+            this.route();
+        }
+        // route() {
+        //     const routeObj = this.routes.find(
+        //         (route) => route.path == location.hash
+        //     );
+        //     const view = routeObj ? routeObj.view : null; // поставить страницу 404
+        // }
+        route() {
+            if (this.currentView) {
+                this.currentView.destroy();
+            }
+            const view = this.routes.find(
+                (route) => route.path == location.hash
+            ).view;
+            this.currentView = new view();
+            this.currentView.render();
+        }
+    }
+
+    new App();
+    // роутинг загрузка вьюшек
+
+})();
